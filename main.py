d425871b90baf125a5a05316bb095d9693c8f7e2
tsukerin <125801694+setusq@users.noreply.github.com>
2023-09-10 16:08:09 +0300

    Update main.py
    Исправлено отображение линии 4*X1 + 5*X2

--- main.py
+++ main.py
@@ -2,18 +2,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x1 = np.linspace(0, 30, 100)
-x2 = np.linspace(0, 30, 100)
+x1 = np.linspace(0, 40, 100)
+x2 = np.linspace(0, 40, 100)
 
 X1, X2 = np.meshgrid(x1, x2)
-
-constraint = 4*X1 + 5*X2
 
 plt.figure(figsize=(8, 6))
 plt.xlabel('x1')
 plt.ylabel('x2')
-contour = plt.contour(X1, X2, constraint, levels=[141], colors='red', linestyles='dashed')
-plt.axhline(color='red', linestyle='dashed', label='4*X1 + 5*X2')
+
+plt.plot(x1, (141 - 4 * x1) / 5, label='4*X1 + 5*X2 = 141', color='red', linestyle='dashed')
+
 plt.axvline(x=19, color='gray', linestyle='dashed', label='x1=19')
 plt.axhline(y=17, color='gray', linestyle='dashed', label='x2=17')
 
