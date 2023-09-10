3232c1b48b852a6dc002e14cbb35058a02ae3756
tsukerin <125801694+setusq@users.noreply.github.com>
2023-09-10 15:37:37 +0300

    Update main.py
    Добавлены линии x и y, а также ограничение

--- main.py
+++ main.py
@@ -6,3 +6,23 @@ x1 = np.linspace(0, 30, 100)
 x2 = np.linspace(0, 30, 100)
 
 X1, X2 = np.meshgrid(x1, x2)
+
+constraint = 4*X1 + 5*X2
+
+plt.figure(figsize=(8, 6))
+plt.xlabel('x1')
+plt.ylabel('x2')
+plt.contour(X1, X2, constraint, levels=[141], colors='red', linestyles='dashed', label='Ограничение')
+
+plt.axvline(x=19, color='gray', linestyle='dashed', label='x1=19')
+plt.axhline(y=17, color='gray', linestyle='dashed', label='x2=17')
+
+plt.axvline(x=19, color='gray', linestyle='dashed', label='x1=19')
+plt.axhline(y=17, color='gray', linestyle='dashed', label='x2=17')
+
+
+
+
+plt.grid(True)
+plt.legend()
+plt.show()
