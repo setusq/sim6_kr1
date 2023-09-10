7ed39e7215c501fa6bc545e7fd1911f4141b78fb
tsukerin <125801694+setusq@users.noreply.github.com>
2023-09-10 16:37:51 +0300

    Update main.py
    Добавлен перпендикуляр у вектору z

--- main.py
+++ main.py
@@ -18,8 +18,9 @@ plt.axhline(y=17, color='gray', linestyle='dashed', label='x2=17')
 
 plt.arrow(0, 0, 3, 5, head_width=0.5, head_length=0.7, fc='black', ec='black', label='вектор z (3, 5)')
 
-
-
+perpendicular_x = np.array([5, -5])
+perpendicular_y = np.array([-3, 3])
+plt.plot(perpendicular_x, perpendicular_y, linestyle='dashed', color='green', label='перпендикуляр')
 
 plt.grid(True)
 plt.legend()
