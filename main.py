5f2d66fe662fdac49bf9fb6387a702ef77cbd2a1
tsukerin <125801694+setusq@users.noreply.github.com>
2023-09-10 15:21:19 +0300

    Update main.py
    Созданы массивы x1 и x2

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 #М3О-234Б-22 Симонова,Матвеенко 6 группа
 import numpy as np
 import matplotlib.pyplot as plt
+
+x1 = np.linspace(0, 30, 100)
+x2 = np.linspace(0, 30, 100)
+
+X1, X2 = np.meshgrid(x1, x2)
